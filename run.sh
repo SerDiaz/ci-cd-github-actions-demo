52f9e3dcfbf7c744ef4e5218c3260e7bb0a3e068
Sergio Diaz <sdiazgon@ull.edu.es>
2024-12-18 12:59:56 +0000

    Generated run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activte the environment
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+# Execute main script
+echo "Running main.py..."
+python src/main.py
+
+# Deactivate the environment
+echo "Deactivating virtual environment..."
+deactivate
