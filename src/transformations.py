486b8e1dfbcb32c4f182bdcb06efd4b1e01bdfa5
Sergio Diaz <sdiazgon@ull.edu.es>
2024-12-18 15:30:31 +0000

    Uncommented funcionalities

--- src/transformations.py
+++ src/transformations.py
@@ -40,81 +40,81 @@ def combine_name_and_surname(
     df[new_col] = df[name_col] + " " + df[surname_col]
     return df
 
-# def generate_unique_id(
-#     df: pd.DataFrame, 
-#     id_col: str, 
-#     prefix: str = "", 
-#     overwrite: bool = True
-# ) -> pd.DataFrame:
-#     """
-#     Generates a unique ID for each row in the DataFrame.
+def generate_unique_id(
+    df: pd.DataFrame, 
+    id_col: str, 
+    prefix: str = "", 
+    overwrite: bool = True
+) -> pd.DataFrame:
+    """
+    Generates a unique ID for each row in the DataFrame.
     
-#     Args:
-#         df (pd.DataFrame): The DataFrame to transform.
-#         id (str): The name of the column to store the generated IDs.
-#         prefix (str, optional): A prefix for the IDs. Defaults to "id_".
-#         overwrite (bool, optional): If True, overwrites the column if it already exists.
-#                                     If False, appends "_new" to the column name. Defaults to True.
+    Args:
+        df (pd.DataFrame): The DataFrame to transform.
+        id (str): The name of the column to store the generated IDs.
+        prefix (str, optional): A prefix for the IDs. Defaults to "id_".
+        overwrite (bool, optional): If True, overwrites the column if it already exists.
+                                    If False, appends "_new" to the column name. Defaults to True.
 
-#     Returns:
-#         pd.DataFrame: The transformed DataFrame with the generated IDs.
-#     """
-#     original_id_col = id_col
-#     if id_col in df.columns:
-#         if overwrite:
-#             df.drop(columns=[id_col], inplace=True)
-#         else:
-#             counter = 1
-#             while id_col in df.columns:
-#                 id_col = f"{original_id_col}_{counter}"
-#                 counter += 1
+    Returns:
+        pd.DataFrame: The transformed DataFrame with the generated IDs.
+    """
+    original_id_col = id_col
+    if id_col in df.columns:
+        if overwrite:
+            df.drop(columns=[id_col], inplace=True)
+        else:
+            counter = 1
+            while id_col in df.columns:
+                id_col = f"{original_id_col}_{counter}"
+                counter += 1
 
-#     df[id_col] = [f"{prefix}{i}" for i in range(1, len(df) + 1)]
-#     return df
+    df[id_col] = [f"{prefix}{i}" for i in range(1, len(df) + 1)]
+    return df
 
-# def add_id_to_sales(
-#     sales_df: pd.DataFrame, 
-#     sellers_df: pd.DataFrame, 
-#     sales_col: str, 
-#     sellers_col: str, 
-#     id_col: str
-# ) -> pd.DataFrame:
-#     """
-#     Adds a seller ID to the sales DataFrame based on a match with the sellers DataFrame.
+def add_id_to_sales(
+    sales_df: pd.DataFrame, 
+    sellers_df: pd.DataFrame, 
+    sales_col: str, 
+    sellers_col: str, 
+    id_col: str
+) -> pd.DataFrame:
+    """
+    Adds a seller ID to the sales DataFrame based on a match with the sellers DataFrame.
     
-#     Args:
-#         sales_df (pd.DataFrame): The sales DataFrame.
-#         sellers_df (pd.DataFrame): The sellers DataFrame.
-#         sales_col (str): The seller column in the sales DataFrame.
-#         sellers_col (str): The seller column in the sellers DataFrame.
-#         id_col (str): The ID column in the sellers DataFrame.
+    Args:
+        sales_df (pd.DataFrame): The sales DataFrame.
+        sellers_df (pd.DataFrame): The sellers DataFrame.
+        sales_col (str): The seller column in the sales DataFrame.
+        sellers_col (str): The seller column in the sellers DataFrame.
+        id_col (str): The ID column in the sellers DataFrame.
 
-#     Returns:
-#         pd.DataFrame: The sales DataFrame with the seller IDs added.
-#     """
-#     sales_df = sales_df.merge(
-#         sellers_df[[sellers_col, id_col]],
-#         left_on=sales_col,
-#         right_on=sellers_col,
-#         how="left"
-#     )
-#     return sales_df
+    Returns:
+        pd.DataFrame: The sales DataFrame with the seller IDs added.
+    """
+    sales_df = sales_df.merge(
+        sellers_df[[sellers_col, id_col]],
+        left_on=sales_col,
+        right_on=sellers_col,
+        how="left"
+    )
+    return sales_df
 
-# def delete_columns(
-#     df: pd.DataFrame, 
-#     columns: list[str]
-# ) -> pd.DataFrame:
-#     """
-#     Deletes the specified columns from a DataFrame.
+def delete_columns(
+    df: pd.DataFrame, 
+    columns: list[str]
+) -> pd.DataFrame:
+    """
+    Deletes the specified columns from a DataFrame.
     
-#     Args:
-#         df (pd.DataFrame): The DataFrame to transform.
-#         columns (list[str]): List of column names to delete.
+    Args:
+        df (pd.DataFrame): The DataFrame to transform.
+        columns (list[str]): List of column names to delete.
 
-#     Returns:
-#         pd.DataFrame: The transformed DataFrame without the specified columns.
-#     """
-#     return df.drop(columns=columns, inplace=False)
+    Returns:
+        pd.DataFrame: The transformed DataFrame without the specified columns.
+    """
+    return df.drop(columns=columns, inplace=False)
 
 # def move_column_to_front(
 #     df: pd.DataFrame, 
