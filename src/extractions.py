f42e93f7d13b0620e21bcb41bbf5e3d5acf336de
Sergio Diaz <sdiazgon@ull.edu.es>
2024-12-18 12:29:25 +0000

    Added read_csv to extractions.py

--- src/extractions.py
+++ src/extractions.py
@@ -11,3 +11,15 @@ def read_json(file_path: str) -> pd.DataFrame:
         pd.DataFrame: A DataFrame containing the JSON data.
     """
     return pd.read_json(file_path)
+
+def read_csv(file_path: str) -> pd.DataFrame:
+    """
+    Reads a CSV file and converts it into a Pandas DataFrame.
+    
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the CSV data.
+    """
+    return pd.read_csv(file_path)
